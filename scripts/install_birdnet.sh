d8136830dafdc6ade085e15e44809d5b0993c00d
Jochen Oberreiter <joberreiter@users.noreply.github.com>
2023-06-11 15:50:09 +0200

    Update install_birdnet.sh
    Add comments and set executable flag for the scripts

--- scripts/install_birdnet.sh
+++ scripts/install_birdnet.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
+#
 # Install BirdNET script
+
 set -x # Debugging
 exec > >(tee -i installation-$(date +%F).txt) 2>&1 # Make log
-set -e # exit installation if anything fails
+set -e # Exit installation if anything fails
 
-my_dir=$HOME/BirdNET-Pi
-export my_dir=$my_dir
-
-cd $my_dir/scripts || exit 1
-
+# Check OS platform
 if [ "$(uname -m)" != "aarch64" ];then
   echo "BirdNET-Pi requires a 64-bit OS.
 It looks like your operating system is using $(uname -m),
@@ -18,7 +16,16 @@ information"
   exit 1
 fi
 
-#Install/Configure /etc/birdnet/birdnet.conf
+# Determine the base BirdNET-Pi diectory
+my_dir=$HOME/BirdNET-Pi
+export my_dir=$my_dir
+
+# Make sure that all scripts are executable
+chmod +x $my_dir/scripts/*.sh || exit 1
+
+# Install & configure the config file /etc/birdnet/birdnet.conf
+# and install the services
+cd $my_dir/scripts || exit 1
 ./install_config.sh || exit 1
 sudo -E HOME=$HOME USER=$USER ./install_services.sh || exit 1
 source /etc/birdnet/birdnet.conf
@@ -31,12 +38,15 @@ install_birdnet() {
   pip3 install -U -r $HOME/BirdNET-Pi/requirements.txt
 }
 
+# Generate the recording directory
 [ -d ${RECS_DIR} ] || mkdir -p ${RECS_DIR} &> /dev/null
 
+# Install the python environment
 install_birdnet
 
+# Install the language labels of the new model
+# according to the configured database language
 cd $my_dir/scripts || exit 1
-
 ./install_language_label_nm.sh -l $DATABASE_LANG || exit 1
 
 exit 0
